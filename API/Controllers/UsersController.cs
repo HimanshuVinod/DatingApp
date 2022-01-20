28c69d490ba39e6844ebd73fcaca2a4954ed2d3d
HimanshuVinod <himanshu.vinod4@gmail.com>
2022-01-20 14:58:25 +0530

    Basic Api Made

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        
+        public readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+           return await _context.User.ToListAsync();
+;
+        }
+
+        //api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.User.FindAsync(id);
+
+        }
+
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } //entity framework will recognise it correctly 
+        public string UserName { get; set; }//for .netcore identity 
+    }
+}
